7fdb490ec62b1a9aa2e746de892ce6af53c66593
thiagorfm <t_moreira@outlook.com>
2018-07-31 23:55:00 -0300

    Setting routes

--- instashop/settings.py
+++ instashop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'instashop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'helloworld/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'hello/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
